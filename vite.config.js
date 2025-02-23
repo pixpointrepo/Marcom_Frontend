68513ae80155f02e033ad3c1c87c16ceab7e6c7e
Rahul <mtal3584@gmail.com>
2025-02-23 17:09:21 +0545

    port number changed

--- vite.config.js
+++ vite.config.js
@@ -7,4 +7,13 @@ export default defineConfig({
     postcss: './postcss.config.js',
   },
   plugins: [react()],
+  server: {
+    port:3003
+    // allowedHosts: [
+    //   '9152-27-34-66-33.ngrok-free.app', // Add your ngrok URL
+    //   'localhost',                     // Allow localhost (important!)
+    //   // Add other hosts if needed, e.g., your local network IP
+    //   // or '*' for all hosts (USE WITH CAUTION IN PRODUCTION)
+    // ],
+  },
 })
