297b460b045e48ebf7e64f96a54f56f68096b0dc
Sanish_Bal <sbalnet369@gmail.com>
2025-04-09 14:26:50 +0545

    api url fix

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = "https://marcom.pixpoint.com.np/api";
 
 
 const getToken = () => localStorage.getItem("adminToken");
