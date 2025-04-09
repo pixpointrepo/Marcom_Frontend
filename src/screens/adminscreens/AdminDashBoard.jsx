9126d0dc2ce75d3982f49060380f685e977a9314
Sanish_Bal <sbalnet369@gmail.com>
2025-04-09 12:30:37 +0545

    fix

--- src/screens/adminscreens/AdminDashBoard.jsx
+++ src/screens/adminscreens/AdminDashBoard.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Sidebar from '../../components/dashboardcomponents/SideBar'
+import Sidebar from '../../components/dashboardcomponents/Sidebar'
 import { Outlet } from 'react-router-dom'
 
 const AdminDashBoard = () => {
